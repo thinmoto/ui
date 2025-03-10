3ba5b3a73a7aa376602735e2af50b3b1cde9d256
thinmoto <thinmoto@gmail.com>
2025-03-10 12:44:56 +0100

    ~ update

--- resources/assets/js/alpine/Modal.js
+++ resources/assets/js/alpine/Modal.js
@@ -23,9 +23,27 @@ export default (element, state = false, modalStateHook) => ({
         let component = this;
 
         if(component.myState)
+        {
+            component.myCounter = document.querySelectorAll('.modal.show').length + 1;
+
             component.myModal.show();
+
+            component.myElement.classList.add('modal_'+(component.myCounter));
+
+            setTimeout(function (){
+                component.myModal._backdrop._element.classList.add('modal_backdrop_'+(component.myCounter));
+            }, 50);
+        }
         else
+        {
             component.myModal.hide();
+
+            component.myElement.classList.remove('modal_'+(component.myCounter));
+
+            setTimeout(function (){
+                component.myModal._backdrop._element.classList.remove('modal_backdrop_'+(component.myCounter));
+            }, 500);
+        }
     },
 
     showLoading() {
@@ -39,29 +57,29 @@ export default (element, state = false, modalStateHook) => ({
             // 'backdrop': 'static',
         });
 
-        component.myElement.addEventListener('show.bs.modal', event => {
-            // let counter = document.querySelectorAll('.modal.show').length;
-
-            component.myElement.classList.add('modal_'+(component.myCounter));
-
-            setTimeout(function (){
-                component.myModal._backdrop._element.classList.add('modal_backdrop_'+(component.myCounter));
-            }, 50);
-        });
-
-        component.myElement.addEventListener('shown.bs.modal', event => {
-            // let counter = document.querySelectorAll('.modal.show').length;
-
-            if(component.myModal._backdrop._element.classList)
-                component.myModal._backdrop._element.classList.add('modal_backdrop_'+(component.myCounter));
-
-            // component.$dispatch('ui-update-state');
-        });
+        // component.myElement.addEventListener('show.bs.modal', event => {
+        //     // let counter = document.querySelectorAll('.modal.show').length;
+        //
+        //     component.myElement.classList.add('modal_'+(component.myCounter));
+        //
+        //     setTimeout(function (){
+        //         component.myModal._backdrop._element.classList.add('modal_backdrop_'+(component.myCounter));
+        //     }, 50);
+        // });
+        //
+        // component.myElement.addEventListener('shown.bs.modal', event => {
+        //     // let counter = document.querySelectorAll('.modal.show').length;
+        //
+        //     if(component.myModal._backdrop._element.classList)
+        //         component.myModal._backdrop._element.classList.add('modal_backdrop_'+(component.myCounter));
+        //
+        //     // component.$dispatch('ui-update-state');
+        // });
 
         component.myElement.addEventListener('hidden.bs.modal', event => {
             component.myState = false;
         });
 
-        component.myCounter = window.editViewCounter = (window.editViewCounter || 0) + 1;
+        // component.myCounter = window.editViewCounter = (window.editViewCounter || 0) + 1;
     }
 })
